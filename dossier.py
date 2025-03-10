0e5c8aedfdbeb233443c6ce536e8a256ae16e865
zied <fazaizied5@gmail.com>
2025-03-10 12:33:49 +0100

    res second confli

--- dossier.py
+++ dossier.py
@@ -6,6 +6,14 @@ def somme(T):
    return S 
 
 Data=[1,3,5]
+<<<<<<< HEAD
 Som=sum(Data)
 print('la somme est :', Som)
 >>>>>>> dossier1
+
+
+    print('la somme est :', sum (Data))
+    print('le min est :', min(Data))
+    print('le max est :', max (Data) 
+ 
+>>>>>>> fb0446a (first mod)
