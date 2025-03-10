0213de7126d84fa9580d9ba7b327b1a8a34b4f4d
zied <fazaizied5@gmail.com>
2025-03-10 12:28:18 +0100

    second mod conflit résoudre

--- dossier.py
+++ dossier.py
@@ -7,13 +7,20 @@ def somme(T):
 
 Data=[1,3,5]
 <<<<<<< HEAD
+<<<<<<< HEAD
 Som=sum(Data)
 print('la somme est :', Som)
 >>>>>>> dossier1
 
 
+
+if Data :
+>>>>>>> edaa063 (second mod)
     print('la somme est :', sum (Data))
     print('le min est :', min(Data))
-    print('le max est :', max (Data) 
+    print('le max est :', max (Data)
+ else:
+    print('dossier vide ')
+
  
 >>>>>>> fb0446a (first mod)
